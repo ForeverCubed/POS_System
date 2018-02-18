641d3f053bcfe53ace32ee2daab295b5aefd80cd
ForeverCubed <forevercubed@gmail.com>
2018-02-17 18:46:42 -0600

    Display Intro
    Start work on the UI (and fixed the major fuckup)

---
+++ src/com/roundtheclock/pos/Main.java
@@ -0,0 +1,39 @@
+package com.roundtheclock.pos;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        GridLayout mainGridLayout = new GridLayout();
+        mainGridLayout.setRows(10);
+        mainGridLayout.setColumns(10);
+        JFrame mainFrame = new JFrame("Konor and Kevin's POS");
+        mainFrame.setSize(800,600);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(true);
+
+
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(mainGridLayout);
+
+        JButton exit = new JButton("Exit");
+        exit.setVisible(true);
+
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mainPanel.add(exit);
+        mainFrame.add(mainPanel);
+
+        mainFrame.setUndecorated(true);
+        mainFrame.setVisible(true);
+    }
+}
